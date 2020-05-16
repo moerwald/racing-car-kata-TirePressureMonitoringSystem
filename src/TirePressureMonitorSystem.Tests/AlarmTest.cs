aeb97d735b216021d107a3cd1c44e03d815c17f3
moerwald <andreas.mewald@gmail.com>
2020-05-16 11:38:07 +0200

    Initial commit of kata

---
+++ src/TirePressureMonitorSystem.Tests/AlarmTest.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    [TestFixture]
+    public sealed class AlarmTest
+    {
+        [TestCase(0.0)]
+        [TestCase(-1.0)]
+        [TestCase(16.99)]
+        public void Check_SensorStatesLowPressure_ReturnTrue(double pressure)
+            => Assert(pressure, true);
+
+        [TestCase(21.01)]
+        [TestCase(1000.0)]
+        public void Check_SensorStatesHighPressure_ReturnTrue(double pressure)
+            => Assert(pressure, true);
+
+        [TestCase(17.0)]
+        [TestCase(17.5)]
+        [TestCase(20.0)]
+        [TestCase(21.0)]
+        public void Check_SensorStatesValidPressure_ReturnFalse(double pressure)
+            => Assert(pressure, false);
+
+        private void Assert(double pressure, bool alarmState)
+        {
+            // Arrange
+            var alarm = new Alarm();
+
+            // Act
+            alarm.Check();
+
+            // Assert
+            NUnit.Framework.Assert.AreEqual(alarmState, alarm.AlarmOn);
+        }
+    }
+}
